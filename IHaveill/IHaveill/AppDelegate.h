778f8078436be7562c7c4fe0808e7fe81bdaac45
赵辰杰 <385101661@qq.com>
2015-05-22 09:33:00 +0800

    I have ill

---
+++ IHaveill/IHaveill/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IHaveill
+//
+//  Created by 赵辰杰 on 15/5/20.
+//  Copyright (c) 2015年 赵辰杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IHaveill/IHaveill/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IHaveill
+//
+//  Created by 赵辰杰 on 15/5/20.
+//  Copyright (c) 2015年 赵辰杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
